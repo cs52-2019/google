41fa9c93c010c2d335cce66c11e4d605d1bb8103
Claire Huang <chuang20@stanford.edu>
2019-04-16 20:13:35 -0700

    Added case and analysis models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'our_app.apps.OurAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend/our_app/models.py
+++ backend/our_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Case(models.Model):
+	status = models.CharField(
+		choices = (('current', 'current'), ('past', 'past')),
+		max_length = 7
+	)
+	title = models.CharField(max_length = 200)
+	image = models.ImageField(upload_to = 'cases')
+
+class Analysis(models.Model):
+	title = models.CharField(max_length = 200)
+	case = models.ForeignKey(Case, on_delete = models.CASCADE)
+	# locations = 
+	time_start = models.DateTimeField()
+	time_end = models.DateTimeField()
+	frequency = models.DurationField()
