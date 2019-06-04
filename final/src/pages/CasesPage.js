838f2a66052f2c42f6ebcefb5cc1cbf61c16be23
Sofia Avila <sofia.m.jamesson@gmail.com>
2019-06-03 17:10:50 -0700

    Beautified home page
    Yeah you heard me

--- final/src/pages/CasesPage.js
+++ final/src/pages/CasesPage.js
@@ -7,20 +7,6 @@ class CasesPage extends React.Component {
     super(props);
   }
 
-  /*
-  | For Sofia:
-  | Go to http://localhost:3000/cases and open the Chrome inspector to get
-  | the Javascript console. It should print out all the cases from the database.
-  | You can use this list to loop over your counter component (just like you do
-  | in counters.jsx) to get dynamically loaded content.
-  |
-  | You and Sasa need to agree on what the database fields will be called so that
-  | when she saves a case, you are able to reference the proper fields to render the info.
-  |
-  | For reference, CasePage does something very similar, except it loads all
-  | analyses for a given case.
-  */
-
   render() {
     return (
       <div>
--- final/src/pages/HomePage.js
+++ final/src/pages/HomePage.js
@@ -9,24 +9,33 @@ class HomePage extends React.Component {
 
   render() {
     return (
-      <div class="container text-center">
-        <div class="row pb-2">
-          <a
-            href={"/cases"}
-            class="btn btn-info btn-lg w-50 mx-auto"
-            role="button"
-          >
-            All Cases
-          </a>
-        </div>
-        <div class="row pb-2">
-          <a
-            href={"/newcase/"}
-            class="btn btn-info btn-lg w-50 mx-auto"
-            role="button"
-          >
-            New Case
-          </a>
+      <div style={{ marginTop: "100px" }}>
+        <div class="container text-center">
+          <img
+            class="mt-5 mb-5"
+            src="https://www.lawctopus.com/wp-content/uploads/2017/12/Accountability-Counsel-Logo-1024x261.jpg"
+            width="240"
+            height="70"
+            alt=""
+          />
+          <div class="row pb-2">
+            <a
+              href={"/cases"}
+              class="btn btn-info btn-lg w-50 mx-auto"
+              role="button"
+            >
+              All Cases
+            </a>
+          </div>
+          <div class="row pb-2">
+            <a
+              href={"/newcase/"}
+              class="btn btn-info btn-lg w-50 mx-auto"
+              role="button"
+            >
+              New Case
+            </a>
+          </div>
         </div>
       </div>
     );
